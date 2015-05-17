4d0833a33d2b976c0a358c8e329a857dfc5b7c5a
gkbsoftware <gkbsoftware@gmail.com>
2015-05-17 14:22:00 -0400

    buttons that are dynamic

--- main.js
+++ main.js
@@ -3,3 +3,23 @@ var button1 = document.getElementById("button1");
 
 var section2 = document.getElementById("section2");
 var button2 = document.getElementById("button2");
+
+var showSection1 = function () {
+	section1.style.display = "block";
+	section2.style.display = "none";
+}
+
+var showSection2 = function () {
+	section1.style.display = "none";
+	section2.style.display = "block";
+}
+
+button1.addEventListener("click", function(e) {
+	showSection2();
+});
+
+button2.addEventListener("click", function(e) {
+	showSection1();
+});
+
+showSection1();
